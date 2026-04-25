c5c8340ad7ec57e694d7bb0b71540b132eadf2c3
carlospfcb <pereiracarlos1993@gmail.com>
2026-04-25 17:10:10 -0400

    fixing some data

--- helpers/testingData.js
+++ helpers/testingData.js
@@ -26,8 +26,8 @@ const testingData = {
     assignLeaveInfo: {
         employeeName: 'Amelia Brown',
         leaveType: 'CAN - Bereavement', 
-        fromDate: '2026-05-05',
-        toDate: '2026-05-05',
+        fromDate: '2026-06-05',
+        toDate: '2026-06-05',
         duration: 'Half Day - Morning',
         comment: 'Testing leave assignment'
     }
